b4d21d6b40ff146a2dacf953e51edc2f3b911e4c
Alex McConnell <alexmcc.dev@gmail.com>
2021-05-16 15:46:25 -0400

    add react strict mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,18 +8,20 @@ import '../styles/index.css' // Import this last
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
-        <SWRConfig
-            value={{
-                fetcher: async (url: string) => {
-                    const res = await axios.get(url)
-                    return res.data
-                },
-            }}
-        >
-            <ModalProvider>
-                <Component {...pageProps} />
-            </ModalProvider>
-        </SWRConfig>
+        <React.StrictMode>
+            <SWRConfig
+                value={{
+                    fetcher: async (url: string) => {
+                        const res = await axios.get(url)
+                        return res.data
+                    },
+                }}
+            >
+                <ModalProvider>
+                    <Component {...pageProps} />
+                </ModalProvider>
+            </SWRConfig>
+        </React.StrictMode>
     )
 }
 
